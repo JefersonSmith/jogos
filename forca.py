08c98798fdc70edab071d05e520056b575617077
CRFlam <jjsmithcrf@gmail.com>
2023-03-12 01:42:35 -0300

    incluindo jogo pedra, papel, tesoura

--- forca.py
+++ forca.py
@@ -5,20 +5,26 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
 
     enforcou = False
     acertou = False
 
+    print("Palavra secreta:", letras_acertadas)
+
     while(not enforcou and not acertou):
 
         chute = input("Qual Letra?")
-        chute = chute.strip()
+        chute = chute.strip() #strip remove os espaços na resposta do usuário
 
         index = 0
         for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Enconotrei a letra {} na posicao {}".format(letra, index))
+            if(chute.upper() == letra.upper()): #upper deixa tudo maiúsculo, caso usuário use um ou outro
+                letras_acertadas[index] = letra
             index = index + 1
+
+        print(letras_acertadas)
 
         print("jogando...")
 
--- jogos.py
+++ jogos.py
@@ -1,12 +1,14 @@
 import forca
 import adivinhacao
+import pedraPapelTesoura
+
 
 def escolha_jogo():
     print("*********************************")
     print("Escolhe o seu jogo!")
     print("*********************************")
 
-    print("(1) Forca (2) Adivinhação")
+    print("(1) Forca (2) Adivinhação (3) Pedra, Papel, Tesoura")
 
     jogo = int(input("Qual jogo?: "))
 
@@ -16,6 +18,9 @@ def escolha_jogo():
     elif(jogo == 2):
         print ("Jogando adivinhação")
         adivinhacao.jogar()
+    elif(jogo ==3):
+        print ("Jogando pedra, papel, tesoura")
+        pedraPapelTesoura.jogar()
 
 if(__name__ == "__main__"):
     escolha_jogo()
